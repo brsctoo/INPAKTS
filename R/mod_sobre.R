5ccd1340ac25514e6a006f4ba68e358872e20241
feyama11 <fytfelipe@gmail.com>
2023-03-21 02:30:16 -0300

    Alterando imagens

--- R/mod_sobre.R
+++ R/mod_sobre.R
@@ -9,113 +9,121 @@
 #' @importFrom shiny NS tagList
 mod_sobre_ui <- function(id) {
   ns <- NS(id)
-  tagList(fluidPage(
-    sidebarLayout(
-      position = "right",
-      sidebarPanel(
-        style = "background-color: #ffffff;",
-        align = "center",
-        width = 5,
-        #Seção de parceiros
-        shinydashboard::infoBox(width = 12, icon=icon(""),
-          tags$h2("Parceria"),
-          tags$img(
-            src = "www/Slide2.png",
-            width = 155,
-            height = 60
-          )
+  tagList(fluidPage(sidebarLayout(
+    position = "right",
+    sidebarPanel(
+      style = "background-color: #ffffff;",
+      align = "center",
+      width = 5,
+      #Seção de parceiros
+      shinydashboard::infoBox(
+        width = 12,
+        icon = icon(""),
+        tags$h2("Parceria"),
+        tags$a(img(
+          src = "www/Slide2.png",
+          width = 250,
+          height = 60
         ),
-        br(),
-        # Seção Financiamento
-        shinydashboard::infoBox(width = 12, icon = icon(""),
-          tags$h2("Financiamento"),
-          tags$ul(
-            tags$img(
-              src = "www/Slide3.png",
-              width = 450,
-              height = 80
-            ),
-            br(),
-            br(),
-            tags$img(
-              src = "www/Slide4.png",
-              width = 200,
-              height = 80
-            ),
-            br(),
-            br(),
-            tags$img(
-              src = "www/Slide5.png",
-              width = 150,
-              height = 80
-            ),
-            br(),
-            br(),
-            tags$img(
-              src = "www/Slide6.png",
-              width = 150,
-              height = 80
-            ),
-            br(),
-            br(),
-            tags$img(
-              src = "www/Slide7.png",
-              width = 150,
-              height = 80
-            )
+        href = "https://www.saude.pr.gov.br/")
+      ),
+      br(),
+      # Seção Financiamento
+      shinydashboard::infoBox(
+        width = 12,
+        icon = icon(""),
+        tags$h2("Financiamento"),
+        tags$ul(
+          tags$img(
+            src = "www/Slide3.png",
+            width = 450,
+            height = 80
+          ),
+          br(),
+          br(),
+          tags$img(
+            src = "www/Slide4.png",
+            width = 200,
+            height = 80
+          ),
+          br(),
+          br(),
+          tags$img(
+            src = "www/Slide5.png",
+            width = 150,
+            height = 80
+          ),
+          br(),
+          br(),
+          tags$img(
+            src = "www/Slide6.png",
+            width = 150,
+            height = 80
+          ),
+          br(),
+          br(),
+          tags$img(
+            src = "www/Slide7.png",
+            width = 150,
+            height = 80
           )
         )
-      ),
-      mainPanel(
-        width = 7,
-        #logo superior
-        tags$img(
+      )
+    ),
+    mainPanel(
+      width = 7,
+      #logo superior
+      tags$a(
+        img(
           src = "www/Slide1.png",
           width = "100%",
           height = 125,
           align = "center"
         ),
-        br(),
-        br(),
-        # Seção Equipe de Desenvolvimento
-        shinydashboard::infoBox(icon = icon("people-group"),
-          width = 12,
-          tags$h2("Equipe de Desenvolvimento"),
-          tags$p("Conheça nossa equipe de desenvolvimento:"),
-          tags$ul(
-            tags$li("Nome do Bolsista 1"),
-            tags$li("Nome do Bolsista 2"),
-            tags$li("Nome do Bolsista 3"),
-            tags$li("Coordenadora: Eniuce Menezes")
-          )
-        ),
-        # Seção Equipe de Colaboradores
-        shinydashboard::infoBox(icon = icon("people-group"),
-          width = 12,
-          tags$h2("Equipe de Colaboradores"),
-          tags$p("Conheça nossos colaboradores: "),
-          tags$ul(
-            tags$li(
-              "SESA-PR: Acácia Maria Lourenço Francisco Nasr; Maria Goretti David Lopes"
-            ),
-            tags$li("DES/UEM: Adrian Strieder Philippsen; Diogo Rossoni"),
-            tags$li("DEM/UEM: Fernanda Nishida, Luciano Andrade"),
-            tags$li("DEF/UEM Raissa Bocchi Pedroso, Rosana Rosseto Oliveira"),
-            tags$li("UNINGÁ:  Sandra Marisa Pelloso"),
-            tags$li("15ª Regional de Saúde-PR: Greicy Cesar do Amaral"),
-            tags$li("Duke University: João Ricardo Nickenig Vissoci")
-          )
+        href = "http://www.des.uem.br/"
+      ),
+      br(),
+      br(),
+      # Seção Equipe de Desenvolvimento
+      shinydashboard::infoBox(
+        icon = icon("people-group"),
+        width = 12,
+        tags$h2("Equipe de Desenvolvimento"),
+        tags$p("Conheça nossa equipe de desenvolvimento:"),
+        tags$ul(
+          tags$li("Nome do Bolsista 1"),
+          tags$li("Nome do Bolsista 2"),
+          tags$li("Nome do Bolsista 3"),
+          tags$li("Coordenadora: Eniuce Menezes")
+        )
+      ),
+      # Seção Equipe de Colaboradores
+      shinydashboard::infoBox(
+        icon = icon("people-group"),
+        width = 12,
+        tags$h2("Equipe de Colaboradores"),
+        tags$p("Conheça nossos colaboradores: "),
+        tags$ul(
+          tags$li(
+            "SESA-PR: Acácia Maria Lourenço Francisco Nasr; Maria Goretti David Lopes"
+          ),
+          tags$li("DES/UEM: Adrian Strieder Philippsen; Diogo Rossoni"),
+          tags$li("DEM/UEM: Fernanda Nishida, Luciano Andrade"),
+          tags$li("DEF/UEM Raissa Bocchi Pedroso, Rosana Rosseto Oliveira"),
+          tags$li("UNINGÁ:  Sandra Marisa Pelloso"),
+          tags$li("15ª Regional de Saúde-PR: Greicy Cesar do Amaral"),
+          tags$li("Duke University: João Ricardo Nickenig Vissoci")
         )
       )
     )
-  ))
+  )))
 }
 
 #' sobre Server Functions
 #'
 #' @noRd
-mod_sobre_server <- function(id){
-  moduleServer( id, function(input, output, session){
+mod_sobre_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
   })
